3419b1ae88c66a957fb8572b0c699b346f231ee6
Eun-Young-lee <poply@hanmail.net>
2022-03-31 13:48:18 +0000

    commit

--- models/user.js
+++ models/user.js
@@ -1,7 +1,14 @@
 const mongoose= require("mongoose");
 
 const userSchema = new mongoose.Schema({
-email: {type: String, unique:true, }
+email: {type: String, unique:true, lowercase:true },
+password: String,
+username: String.apply,
+gender: {
+    type: String,
+    enum: ['MALE', 'FEMALE']
+},
+PHONE: Number
 })
 
 module.exports=mongoose.model('User', userSchema);
