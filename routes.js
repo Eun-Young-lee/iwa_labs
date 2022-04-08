7746efe2db8c13e64f8fced1ef1e27e4b0150e4e
Eun-Young-lee <poply@hanmail.net>
2022-04-08 08:14:08 +0000

    commit

--- routes.js
+++ routes.js
@@ -3,12 +3,12 @@ const express=require("express"),
       itemCtrl =require("./item-controller"),
       userCtrl = require("./user-controller");
 
-    router.get("/:foo/:bar", itemCtrl.helloWorld);
+    //router.get("/:foo/:bar", itemCtrl.helloWorld);
     router.post('/users', userCtrl.createUser); 
     router.get('/users', userCtrl.getUsers); 
     router.get('/users/:id', userCtrl.getUser); 
     router.put('/users/:id', userCtrl.updateUser); 
-
+    router.delete('/users/:id', userCtrl.deleteUser); 
     
 module.exports = router;
 
